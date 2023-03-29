104a5b6866f6cade27c31c1c5892bc5dd1e0d1d1
gc2025 <gabrieldcarson@gmail.com>
2023-03-29 19:11:55 -0400

    In=class Example 3/29

--- in-class-examples/modularSiteStart/tour/index.php
+++ in-class-examples/modularSiteStart/tour/index.php
@@ -5,11 +5,20 @@
 
 	include($path . "assets/inc/header.php");
 	include($path . "assets/inc/nav.php");
-	
+
+	include($path . "../dbcon.php");
+
+	$sql = "SELECT * FROM `240_modular_content` WHERE `name` = '". $title ."' LIMIT 1";
+
+	$result = $conn->query($sql);
+
+	while($row = $result->fetch_assoc() ){
+		echo $row['content'];
+
+	}
 ?>
 
-	<h1>Tour page</h1> 
-	<div>This is my Test</div>
+	
 
 <?php
     include($path . "assets/inc/footer.php");
