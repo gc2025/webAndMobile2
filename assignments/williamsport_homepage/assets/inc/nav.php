980affde5e6d215972cbdd14550a52fcb337036d
GDC2964 <CARSONGABRIEL2021@GMAIL.COM>
2023-04-01 18:29:48 -0400

    Updated and fixed navigation

--- assignments/williamsport_homepage/assets/inc/nav.php
+++ assignments/williamsport_homepage/assets/inc/nav.php
@@ -1,24 +1,24 @@
 <!-- The Nav bar will be the same all across the web page -->
-<img src="assets/pictures/williamsportSeal_100px.png" alt="Seal of Williamsport" class="sealOfWill">
+<img src="<?php echo $path?>assets/pictures/williamsportSeal_100px.png" alt="Seal of Williamsport" class="sealOfWill">
 <nav class="dropdown">
         
     <ul>
-        <li><a href="<?php echo $path ?>/index.html">Home</a></li>
+        <li><a href="<?php echo $path ?>index.php">Home</a></li>
 
         <li>Shopping
             <ul class="submenu">
-                <li><a href="<?php echo $path ?>/shopping/localResturants/index.html">Local Resturants</a></li>
-                <li><a href="<?php echo $path ?>/shopping/localShops/index.html">Local Shops</a></li>
-                <li><a href="<?php echo $path ?>/shopping/localTheater/index.html">Theater/Concert Venues</a> </li>
+                <li><a href="<?php echo $path ?>localResturants/index.php">Local Resturants</a></li>
+                <li><a href="<?php echo $path ?>localShops/index.php">Local Shops</a></li>
+                <li><a href="<?php echo $path ?>localTheater/index.php">Theater/Concert Venues</a> </li>
             </ul>
 
         </li>
 
         <li>Activities
             <ul class="submenu">
-                <li><a href="<?php echo $path ?>/hiking/index.php">Hiking</a></li>
-                <li><a href="<?php echo $path ?>/reptileLand/index.php">Reptile land</a></li>
-                <li><a href="<?php echo $path ?>/littleLeaugeM/index.php">Little Leauge Museum</a></li>
+                <li><a href="<?php echo $path ?>hiking/index.php">Hiking</a></li>
+                <li><a href="<?php echo $path ?>reptileLand/index.php">Reptile land</a></li>
+                <li><a href="<?php echo $path ?>littleLeaugeM/index.php">Little Leauge Museum</a></li>
 
             </ul>
 
@@ -26,10 +26,10 @@
 
         <li>Landmarks
             <ul class="submenu">
-                <li><a href="<?php echo $path ?>/baldEagle/index.php">Eagle State Park</a></li>
-                <li><a href="<?php echo $path ?>/pineC/index.php">Pine Creek State Park</a> </li>
-                <li><a href="<?php echo $path ?>/loyalS/index.php">Loyalsock State Park</a></li>
-                <li><a href="<?php echo $path ?>/susqueP/index.php">Susquehanna State Park</a></li>
+                <li><a href="<?php echo $path ?>baldEagle/index.php">Eagle State Park</a></li>
+                <li><a href="<?php echo $path ?>pineC/index.php">Pine Creek State Park</a> </li>
+                <li><a href="<?php echo $path ?>loyalS/index.php">Loyalsock State Park</a></li>
+                <li><a href="<?php echo $path ?>susqueP/index.php">Susquehanna State Park</a></li>
 
             </ul>
 
@@ -37,8 +37,8 @@
 
         <li>History
             <ul class="submenu">
-                <li><a href="<?php echo $path ?>/keyEvents/index.php">Key Events in Williamsport</a></li>
-                <li><a href="<?php echo $path ?>/millionaireRow/index.php">Millionaire Row</a></li>
+                <li><a href="<?php echo $path ?>keyEvents/index.php">Key Events in Williamsport</a></li>
+                <li><a href="<?php echo $path ?>millionaireRow/index.php">Millionaire Row</a></li>
 
             </ul>
 
@@ -47,7 +47,7 @@
         <li>Contact
             <ul class="submenu">
                 <li><a href="https://cityofwilliamsport.org/">City Website</a></li>
-                <li><a href="<?php echo $path ?>/menu_forms/index.php">Feedback</a></li>
+                <li><a href="<?php echo $path ?>menu_forms/index.php">Feedback</a></li>
             </ul>
         </li>
 
