660243bc7e112554bebcb8c8ae6873bba44eca6d
gc2025 <gabrieldcarson@gmail.com>
2023-04-25 20:16:21 -0400

    in-class Example (4/24)

--- in-class-examples/ajaxstart/addUser.php
+++ in-class-examples/ajaxstart/addUser.php
@@ -4,6 +4,30 @@
 	* Return false if connection failed.
 	*/
 
+	include("../dbcon.php");
+
+	if(!empty($_GET['first']) && !empty($_GET['last'])){
+		$sql = "INSERT INTO `240Ajax2198` (`first`, `last`) VALUES (?, ?);";
+
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("ss", $_GET['first'], $_GET['last']);
+		$stmt->execute();
+		$stmt->close();
+
+	}
+
+	$sql = "SELECT * FROM `240Ajax2198` LIMIT 50 ";
+	
+	$result = $conn->query($sql);
+	
+	$nameArray = array();
+	while($row = $result->fetch_assoc()){
+		//echo $row['first'] . "<br/>";
+		$nameArray[] = $row;
+
+	}
+
+	//var_dump($nameArray);
 
 	//Code header() is to make sure we get new data - clears the cache!
 	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
@@ -12,5 +36,7 @@
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
 	header('Content-Type: application/json');
+	
+	echo json_encode($nameArray);
 
 ?>
--- in-class-examples/ajaxstart/index.php
+++ in-class-examples/ajaxstart/index.php
@@ -22,16 +22,64 @@
 		// We create the HTTP Object
 		var http = getHTTPObject();
 
+		function checkUsers(){
+			if(http){
+				http.open("GET", "addUser.php", true);
+				http.onreadystatechange = handleHttpResponse;
+				http.send(null);
+
+				window.setTimeout(checkUsers, 1500);
+
+			}
+
+		}
+
+		function handleHttpResponse(){
+			if(http.readyState == 4){
+				//The Response is ready
+				var json_obj = JSON.parse(http.responseText);
+				console.log(json_obj);
+				dealData(json_obj);
+			}
+
+		}
+
+		function dealData(json_obj){
+			var myData = "";
+
+			for(var i = 0; i < json_obj.length; i++){
+				myData += json_obj[i].first + " " + json_obj[i].last + " ";
+				console.log(myData[i]);
+
+			}
+
+			document.getElementById("list").innerHTML = myData;
+
+		}
+
+		function addUser(){
+			let first = document.getElementById("first").value;
+			let last = document.getElementById("last").value;
+
+			if(http){
+				http.open("GET", "addUser.php?first=" + first + "&last=" + last);
+				http.onreadystatechange = handleHttpResponse;
+				http.send(null);
+
+			}
+
+		}
+
 
 	</script>
 </head>
-<body onload="">
+<body onload="checkUsers()">
 	<h3>The List</h3>
 	<div id="list"></div>
 	<hr/>
 	<h3>Add to the list</h3>
 	First name: <input type="text" id="first" />
 	Last name: <input type="text" id="last" />
-	<button type="button" onclick="">Add to the List </button>
+	<button type="button" onclick="addUser()">Add to the List </button>
 </body>
 </html>
